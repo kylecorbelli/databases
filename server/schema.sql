0dcca4b0d81b95147a6ea4530ee526a389748b67
pacificLiving <kyle.corbelli@gmail.com>
2016-02-05 16:15:50 -0800

    Create schemas for each table

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,35 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  ID int AUTO_INCREMENT,
+  Name varchar(40),
+  PRIMARY KEY(ID)
+);
+
+CREATE TABLE rooms (
+  ID int AUTO_INCREMENT,
+  Name varchar(40),
+  PRIMARY KEY(ID)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  ID int AUTO_INCREMENT,
+  Text varchar(140),
+  UserID int,
+  Timestamp timestamp,
+  RoomID int,
+  PRIMARY KEY(ID),
+  FOREIGN KEY(UserID) REFERENCES users(ID),
+  FOREIGN KEY(RoomID) REFERENCES rooms(ID) 
+);
+
+
+CREATE TABLE users_rooms (
+  UserID int,
+  RoomID int,
+  FOREIGN KEY(UserID) REFERENCES users(ID),
+  FOREIGN KEY(RoomID) REFERENCES rooms(ID) 
 );
 
 /* Create other tables and define schemas for them here! */
